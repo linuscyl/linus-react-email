3f3ab7ad4b851445f03ac43647cb1cd15ee4a77a
Linus <linuschan@joyaether.com>
2019-12-17 20:02:58 +0800

    success in dropdown list

--- src/App.js
+++ src/App.js
@@ -57,6 +57,12 @@ class App extends Component {
           <input type="email" name="email" />
           <label>Message</label>
           <textarea name="message" />
+          <select name="cars">
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="fiat">Fiat</option>
+            <option value="audi">Audi</option>
+          </select>
           <input type="submit" value="Send" />
         </form>
         }
