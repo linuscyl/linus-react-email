06806913016197ea6a6e43b39327029206b49a7d
Linus <linuschan@joyaether.com>
2019-12-24 14:48:42 +0800

    update checkbox

--- src/Result.js
+++ src/Result.js
@@ -11,6 +11,13 @@ class Result extends Component {
         <p>Your form is submitted!</p>
         <p>{this.props.name}</p>
         <p><span style={{ color: "orange", fontSize: "20px" }}>目標人物</span>已於{this.props.restaurant}恭侯，請於15mins內到達{this.props.restaurant}</p>
+        <hr />
+        <h3>溫馨提示: </h3>
+        <input type="checkbox" /> 防曬 <br />
+        <input type="checkbox" /> 鎖匙 <br />
+        <input type="checkbox" /> 身份證 <br />
+        <input type="checkbox" /> 錢 <br />
+        <input type="checkbox" /> 纸巾 <br />
       </div>
     )
   }
